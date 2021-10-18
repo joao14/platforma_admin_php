548eace2211a5c9279083e0fefa59b8ba7e4df64
Alexander Merino <gamerino@pichincha.com>
2021-10-17 23:07:21 -0500

    Added new configurations

--- db/db_conn.php
+++ db/db_conn.php
@@ -4,7 +4,7 @@ function OpenCon(){
  
 $dbhost = "localhost";
 $dbuser = "root";  
-$dbpass = "M4rk3t1ng";   
+$dbpass = "root";   
 $db = "plataforma_admin";   
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
